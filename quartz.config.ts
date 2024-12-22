e2b4740a582ae416566faf9f7057cd463810efb8
vicviod <vicviodlogin@gmail.com>
2024-12-22 17:28:41 -0500

    Quartz sync: Dec 22, 2024, 5:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Open Sans",
+        body: "EB Garamond",
         code: "IBM Plex Mono",
       },
       colors: {
