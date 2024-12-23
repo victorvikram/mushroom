dde80ee21e1760491645c718b7471112aeb52bd2
vicviod <vicviodlogin@gmail.com>
2024-12-22 20:17:37 -0500

    Quartz sync: Dec 22, 2024, 8:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "Excalidraw", "README"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Excalidraw", "README.md"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
